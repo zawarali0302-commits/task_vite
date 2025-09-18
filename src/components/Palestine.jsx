35a46c4e753261bbb33617771e6c2d9a69b47247
zawarali0302-commits <zawarali0302@gmail.com>
2025-09-18 07:36:56 -0700

    Update Palestine.jsx

--- src/components/Palestine.jsx
+++ src/components/Palestine.jsx
@@ -7,7 +7,7 @@ const Palestine = () => {
         <div className="border h-52 w-72 grid grid-row-3 relative">
           <div className="bg-black"></div>
           <div className='bg-[#267c23] row-start-3'></div>
-          <div className=' text-red-700 w-0 h-0 border-t-[104px] border-b-[104px] border-l-[80px] border-t-transparent border-b-transparent border-l-red-500 absolute top-0'>
+          <div className='w-0 h-0 border-t-[104px] border-b-[104px] border-l-[80px] border-t-transparent border-b-transparent border-l-red-500 absolute top-0'>
           </div>
         </div>
 
