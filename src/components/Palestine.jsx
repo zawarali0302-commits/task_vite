77b756502e39e373264e89f251555ca4a4ddce65
zawarali0302-commits <zawarali0302@gmail.com>
2025-09-18 07:09:04 -0700

    Update Palestine.jsx

--- src/components/Palestine.jsx
+++ src/components/Palestine.jsx
@@ -4,18 +4,18 @@ const Palestine = () => {
   return (
     <div>
       <div className="min-h-screen grid place-content-center">
-        <h1>flag</h1>
-        <div className="border h-52 w-72 grid grid-row-3 grid-cols-6">
-          <div className="bg-red-500"></div>
-          <div className="bg-black col-start-2 col-span-5"></div>
-          <div className="bg-red-500"></div>
-          <div className="bg-red-500"></div>
-          <div className="bg-red-500 row-start-3"></div>
-          <div className='bg-[#267c23] col-start-2 row-start-3 col-span-5 '></div>
+        <div className="border h-52 w-72 grid grid-row-3 relative">
+          <div className="bg-black"></div>
+          <div className='bg-[#267c23] row-start-3'></div>
+          <div className=' text-red-700 w-0 h-0 border-t-[104px] border-b-[104px] border-l-[80px] border-t-transparent border-b-transparent border-l-red-500 absolute top-0'>
+          </div>
         </div>
 
-
       </div>
+
+
+
+
     </div>
 
   )
